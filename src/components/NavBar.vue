45859777cdc23031c991201695dac29c24b7e031
Piccolo <kabiryusufbashir@gmail.com>
2022-03-10 17:28:51 +0100

    Create MobileScreen NavBar Component

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,16 +1,23 @@
 <template>
+    <!-- larger Screen  -->
     <div id="nav" class="hidden md:grid grid-cols-6 gap-2 px-14 py-7">
         <LargerScreen />
+    </div>
+    <!-- Mobile Screen  -->
+    <div id="nav" class="md:hidden px-4 py-2 mb-4 text-sm fixed">
+        <MobileScreen />
     </div>
 </template>
 
 <script>
     import LargerScreen from "@/components/nav/LargerScreen.vue"
+    import MobileScreen from "@/components/nav/MobileScreen.vue"
     
     export default {
         name: "NavBar",
         components: {
             LargerScreen,
+            MobileScreen
         }
     }
 </script>
