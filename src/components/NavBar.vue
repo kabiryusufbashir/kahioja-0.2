9cfd073205317c3ab12de44a3d73abb0e32cc130
Piccolo <kabiryusufbashir@gmail.com>
2022-03-10 17:34:21 +0100

    Create BottomNavBar component

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -7,17 +7,23 @@
     <div id="nav" class="md:hidden px-4 py-2 mb-4 text-sm fixed">
         <MobileScreen />
     </div>
+    <!-- Bottom Nav  -->
+    <div id="nav-bottom" class="md:hidden">
+        <BottomNavBar />
+    </div>
 </template>
 
 <script>
     import LargerScreen from "@/components/nav/LargerScreen.vue"
     import MobileScreen from "@/components/nav/MobileScreen.vue"
+    import BottomNavBar from "@/components/nav/BottomNavBar.vue"
     
     export default {
         name: "NavBar",
         components: {
             LargerScreen,
-            MobileScreen
+            MobileScreen,
+            BottomNavBar
         }
     }
 </script>
