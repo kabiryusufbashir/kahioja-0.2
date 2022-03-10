cfd2a3b8971c376b4d2b45d5d66c65558a3793a0
Piccolo <kabiryusufbashir@gmail.com>
2022-03-10 17:20:14 +0100

    Create LargerScreen NavBar component

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,22 +1,16 @@
 <template>
     <div id="nav" class="hidden md:grid grid-cols-6 gap-2 px-14 py-7">
-        <Logo />
-        <SearchBar />
-        <CartBar />
+        <LargerScreen />
     </div>
 </template>
 
 <script>
-    import Logo from "@/components/nav/Logo.vue"
-    import SearchBar from "@/components/nav/SearchBar.vue"
-    import CartBar from "@/components/nav/CartBar.vue"
-
+    import LargerScreen from "@/components/nav/LargerScreen.vue"
+    
     export default {
         name: "NavBar",
         components: {
-            Logo,
-            SearchBar,
-            CartBar
+            LargerScreen,
         }
     }
 </script>
---
+++ src/components/nav/LargerScreen.vue
@@ -0,0 +1,22 @@
+<template>
+    <div id="nav" class="hidden md:grid grid-cols-6 gap-2 px-14 py-7">
+        <Logo />
+        <SearchBar />
+        <CartBar />
+    </div>
+</template>
+
+<script>
+    import Logo from "@/components/nav/Logo.vue"
+    import SearchBar from "@/components/nav/SearchBar.vue"
+    import CartBar from "@/components/nav/CartBar.vue"
+
+    export default {
+        name: "LargerScreen",
+        components: {
+            Logo,
+            SearchBar,
+            CartBar
+        }
+    }
+</script>
