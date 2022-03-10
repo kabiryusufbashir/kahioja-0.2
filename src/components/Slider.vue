fa24a5734dd07246c53b6275ab60c57729d903af
Piccolo <kabiryusufbashir@gmail.com>
2022-03-10 15:29:24 +0100

    Create Slider Component

---
+++ src/components/Slider.vue
@@ -0,0 +1,30 @@
+<template>
+    <div id="banner" class="slider relative top-36 md:top-0 mt-64 md:mt-6">
+        <div class="flex items-center">
+            <div class="slideshow-container">
+                <div class="mySlides fade">
+                    <img src="../assets/images/banner-1.jpg" style="width:100%">
+                </div>
+                
+                <div class="mySlides fade">
+                    <img src="../assets/images/banner-2.jpg" style="width:100%">
+                </div>
+
+                <div class="mySlides fade">
+                    <img src="../assets/images/banner-3.jpg" style="width:100%">
+                </div>
+            </div>
+        </div>
+        <div class="flex flex-row justify-center items-center mt-8 mx-auto">    
+            <span class="dot"></span> 
+            <span class="dot"></span> 
+            <span class="dot"></span> 
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+       name: "Slider"
+    }
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,19 @@
 <template>
   <div>
     <NavBar />
+    <Slider />
   </div>
 </template>
 
 <script>
-import NavBar from "@/components/NavBar.vue";
+import NavBar from "@/components/NavBar.vue"
+import Slider from "@/components/Slider.vue"
 
 export default {
   name: "Home",
   components: {
     NavBar,
+    Slider
   },
 };
 </script>
